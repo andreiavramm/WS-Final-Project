185b1b1933aee7fe6ddcf02bd38e13a355721fa7
andreiavramm <andreiavram88@gmail.com>
2020-05-23 13:02:26 +0300

    added funtionality to scrolling and fixed gallery

--- js/script.js
+++ js/script.js
@@ -26,8 +26,68 @@
         })
     }
 
-    const scrollToSection = () => {
+    const scrollToSection = sectionId => {
+        let sectionPosition, sectionOffset;
+        const navigationHeight = document.querySelector('header nav').offsetHeight;
+        const pageWidth = window.innerWidth;
 
+        if (sectionId !== '#') {
+            sectionOffset = document.querySelector(sectionId).offsetTop;
+            sectionPosition = pageWidth > mobileWidth ? sectionOffset - navigationHeight : sectionOffset;
+        } else {
+            sectionPosition = 0;
+        }
+
+        window.scrollTo({
+            'behavior': 'smooth',
+            'left': 0,
+            'top': sectionPosition
+        })
+    }
+
+    const onTestimonialChange = () => {
+        let lastChild, firstChild;
+        const prevArrow = document.querySelector('#ct-testimonials-previous');
+        const nextArrow = document.querySelector('#ct-testimonials-next');
+        const testimonials = document.querySelector('.ct-testimonials ul');
+
+        document.addEventListener('click', () => {
+            if(event.target === prevArrow) {
+                lastChild = testimonials.lastElementChild;
+                testimonials.insertAdjacentElement('afterbegin', lastChild);
+            } else if (event.target === nextArrow) {
+                firstChild = testimonials.firstElementChild;
+                testimonials.insertAdjacentElement('beforeend', firstChild);
+            }
+        })
+    }
+
+    const onGalleryImageClick = () => {
+        const galleryImageList = document.querySelectorAll('#ct-gallery li');
+        const galleryImages = [...galleryImageList];
+
+        galleryImages.forEach(image => {
+            image.addEventListener('click', event => {
+                galleryImageOpen(event.target);
+            })
+        })
+    }
+
+    const galleryImageOpen = (image) => {
+        const imageSrc = image.getAttribute('src');
+        const openedImage = `<div class="ct-backdrop"><img src="${imageSrc}" alt="" />
+                            <span class="ct-backdrop-close">X</span></div>`;
+        document.body.insertAdjacentHTML('beforeend', openedImage);
+        galleryImageClose();
+    }
+
+    const galleryImageClose = () => {
+        const closeButton = document.querySelector('.ct-backdrop-close');
+
+        closeButton.addEventListener('click', () => {
+            const backdrop = document.querySelector('.ct-backdrop');
+            backdrop.remove();
+        })
     }
 
     window.addEventListener('scroll', () => {
@@ -35,6 +95,8 @@
     })
 
     onNavItemClick();
+    onTestimonialChange();
+    onGalleryImageClick();    
 
 })();
 
